b50e0e921bb28f6704743db85b152262eccb026d
bryanlo22 <bryanlo22@gmail.com>
2025-04-20 01:25:20 -0700

    add color system

--- app.vue
+++ app.vue
@@ -1,6 +1,269 @@
+<script lang="ts" setup>
+import { ref } from "vue";
+import chroma from "chroma-js";
+
+const colorSteps = ref(7);
+const colors: Ref<string[]> = ref([]);
+const showHex = ref(false);
+const showHSL = ref(false);
+const showLCH = ref(false);
+const showContrast = ref(false);
+
+onMounted(() => {
+  const initialColorCount = 5;
+  const initialHue = randomHue();
+
+  for (let i = 0; i < initialColorCount; i++) {
+    const hue = initialHue + i * (360 / (initialColorCount + 1));
+    const color = randomColor(hue % 360);
+    colors.value.push(color.hex());
+  }
+});
+
+function randomHue() {
+  return Math.floor(Math.random() * 360);
+}
+
+function randomColor(hue: number) {
+  return chroma.hcl(hue, 40 + Math.random() * 10, 50);
+}
+
+function lightest(hex: string) {
+  const hue = chroma(hex).hcl()[0];
+  return chroma.hcl(hue, 30, 101).hex();
+}
+
+function darkest(hex: string) {
+  const hue = chroma(hex).hcl()[0];
+  return chroma.hcl(hue, 20, 1).hex();
+}
+
+function scaled(hex: string) {
+  return chroma
+    .scale([lightest(hex), chroma(hex).set("hcl.l", 52), darkest(hex)])
+    .colors(colorSteps.value);
+}
+
+function getContrastColor(j: number, baseHex: string) {
+  if (j < colorSteps.value / 2) {
+    return darkest(baseHex);
+  } else {
+    return lightest(baseHex);
+  }
+}
+
+function getContrastValue(j: number, baseHex: string, currentHex: string) {
+  return (
+    Math.round(
+      100 * chroma.contrast(currentHex, getContrastColor(j, baseHex))
+    ) / 100
+  );
+}
+
+function toPercentage(value: number) {
+  return Math.round(100 * value);
+}
+
+function getHSL(hex: string) {
+  const [h, s, l] = chroma(hex).hsl();
+  return `hsl(${Math.round(h)}deg ${toPercentage(s)}% ${toPercentage(l)}%)`;
+}
+
+function getLCH(hex: string) {
+  const [l, c, h] = chroma(hex).lch();
+  return `lch(${Math.round(l)}% ${Math.round(c)} ${Math.round(h)})`;
+}
+</script>
+
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+  <div class="page">
+    <div class="controls">
+      <label>
+        Color Steps:
+        <input
+          v-model="colorSteps"
+          type="number"
+          @input="
+            () => {
+              if (colorSteps < 5) {
+                colorSteps = 5;
+              }
+              if (colorSteps > 9) {
+                colorSteps = 9;
+              }
+              colorSteps = Math.floor(colorSteps);
+            }
+          "
+        />
+      </label>
+      <label>
+        <input v-model="showHex" type="checkbox" />
+        Hex
+      </label>
+      <label>
+        <input v-model="showHSL" type="checkbox" />
+        HSL (Hue, Saturation, Lightness)
+      </label>
+      <label>
+        <input v-model="showLCH" type="checkbox" />
+        HCL (Hue, Chroma, Lightness)
+      </label>
+      <label>
+        <input v-model="showContrast" type="checkbox" />
+        WCAG Contrast Ratio
+      </label>
+      <button
+        v-if="colors.length < 9"
+        @click="() => colors.push(randomColor(randomHue()).hex())"
+      >
+        Add Color
+      </button>
+    </div>
+    <div class="color-grid">
+      <div v-for="(baseHex, i) in colors" :key="i" class="colors">
+        <input
+          :value="baseHex"
+          type="color"
+          @input="
+            (event) => {
+              const target = event?.target as HTMLInputElement;
+              if (target?.value) {
+                colors[i] = target.value;
+              }
+            }
+          "
+        />
+        <template v-for="(currentHex, j) in scaled(baseHex)" :key="j">
+          <div
+            class="color"
+            :style="{
+              backgroundColor: currentHex,
+              color: getContrastColor(j, baseHex),
+            }"
+          >
+            <div v-if="showHex">
+              {{ currentHex }}
+            </div>
+            <div v-if="showHSL">
+              {{ getHSL(currentHex) }}
+            </div>
+            <div v-if="showLCH">
+              {{ getLCH(currentHex) }}
+            </div>
+            <div v-if="showContrast">
+              {{ getContrastValue(j, baseHex, currentHex) }}
+            </div>
+          </div>
+        </template>
+        <button
+          class="submit"
+          :style="{
+            backgroundColor: scaled(baseHex).at(-2),
+            color: lightest(baseHex),
+          }"
+          @mouseover="
+            (event) => {
+              const target = event?.target as HTMLButtonElement;
+              if (target?.style) {
+                target.style.backgroundColor = scaled(baseHex).at(-3) || '';
+              }
+            }"
+          @mouseout="
+              (event) => {
+                const target = event?.target as HTMLButtonElement;
+                if (target?.style) {
+                  target.style.backgroundColor = scaled(baseHex).at(-2) || '';
+                }
+              }"
+        >
+          Submit
+        </button>
+        <div
+          class="tag"
+          :style="{
+            backgroundColor: scaled(baseHex).at(1),
+            color: scaled(baseHex).at(-2),
+          }"
+        >
+          Tag
+        </div>
+      </div>
+    </div>
   </div>
 </template>
+
+<style scoped>
+.page {
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  font-family: "Open Sans", sans-serif;
+  font-size: 16px;
+}
+
+.controls {
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+}
+
+label {
+  width: fit-content;
+  display: flex;
+  align-items: center;
+  gap: 5px;
+}
+
+input[type="number"] {
+  width: 50px;
+}
+
+input[type="checkbox"] {
+  margin: 0;
+}
+
+.color-grid {
+  display: flex;
+  justify-content: space-evenly;
+}
+
+.colors {
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+}
+
+input[type="color"] {
+  width: 100%;
+}
+
+.color {
+  width: 100%;
+  min-height: 100px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 5px;
+  text-align: center;
+}
+
+.submit {
+  height: 50px;
+  border: none;
+  border-radius: 20px;
+  cursor: pointer;
+  margin: 10px;
+  font-family: "Open Sans", sans-serif;
+  font-size: 16px;
+  font-weight: 700;
+}
+
+.tag {
+  margin: 0 auto;
+  padding: 5px 15px;
+  font-size: 14px;
+  font-weight: 700;
+  border-radius: 10px;
+}
+</style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
+  app: {
+    head: {
+      title: "Color System",
+    },
+  },
+  compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint']
-})
+  modules: ["@nuxt/eslint", "@nuxt/fonts"],
+});
